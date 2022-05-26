ac195d948438903827cc7804293faab7ddf01a7a
Joshua <joshua.weinbrenner@rwth-aachen.de>
2022-05-26 15:13:40 +0200

    Updated slurmjob.sh

--- slurmjob.sh
+++ slurmjob.sh
@@ -8,6 +8,9 @@
  
 # declare the merged STDOUT/STDERR file
 #SBATCH --output=output.%J.txt
- 
+
+export CONDA_ROOT=$HOME/miniconda3
+. $CONDA_ROOT/etc/profile.d/conda.sh
+export PATH="$CONDA_ROOT/bin:$PATH" 
 ### begin of executable commands
-python3 /home/up087847/DLLab/SlurmTest.py
+/home/up087847/miniconda3/envs/dllab/bin/python /home/up087847/DLLab/SlurmTest.py
