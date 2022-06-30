e969a03eb6edbf5f83aee7e735c1583da1ae74f3
Joshua <joshua.weinbrenner@gmail.com>
2022-06-30 23:28:50 +0200

    discarded preprocessing steps

--- raytuning.py
+++ raytuning.py
@@ -15,52 +15,13 @@ from ray import tune
 from ray.tune.integration.xgboost import TuneReportCheckpointCallback
 
 
-df = pd.read_csv(r"/home/up087847/DLLab/train.csv", low_memory=False)
-columns = list(df.columns)
+preprocessed=pd.read_csv('preprocessed.csv')
+y = preprocessed.columns[1]
+x = preprocessed.columns[2:]
 
-f_target, f_home_team_name, f_away_team_name, f_match_date = [[feature] for feature in columns[1:5]]
-f_league_name, f_league_id, f_is_cup, f_home_team_coach_id, f_away_team_coach_id = [[feature] for feature in columns[5:10]]
+Y=preprocessed[y]
+X=preprocessed[x]
 
-f_home_team_history_match_date = columns[10:20]
-f_home_team_history_is_play_home = columns[20:30]
-f_home_team_history_is_cup = columns[30:40]
-f_home_team_history_goal = columns[40:50]
-f_home_team_history_opponent_goal = columns[50:60]
-f_home_team_history_rating = columns[60:70]
-f_home_team_history_opponent_rating = columns[70:80]
-f_home_team_history_coach = columns[80:90]
-f_home_team_history_league_id = columns[90:100]
-f_away_team_history_match_date = columns[100:110]
-f_away_team_history_is_play_home = columns[110:120]
-f_away_team_history_is_cup = columns[120:130]
-f_away_team_history_goal = columns[130:140]
-f_away_team_history_opponent_goal = columns[140:150]
-f_away_team_history_rating = columns[150:160]
-f_away_team_history_opponent_rating = columns[160:170]
-f_away_team_history_coach = columns[170:180]
-f_away_team_history_league_id = columns[180:190]
-
-features_shitty = f_match_date + f_home_team_coach_id + f_away_team_coach_id + f_home_team_history_match_date + f_home_team_history_coach + f_away_team_history_match_date + f_away_team_history_coach
-features_boolean = f_is_cup + f_home_team_history_is_play_home + f_home_team_history_is_cup + f_away_team_history_is_play_home + f_away_team_history_is_cup
-features_numerical = f_home_team_history_goal + f_home_team_history_opponent_goal + f_home_team_history_rating + f_home_team_history_opponent_rating + f_away_team_history_goal + f_away_team_history_opponent_goal + f_away_team_history_rating + f_away_team_history_opponent_rating
-features_categorical = f_home_team_name + f_away_team_name + f_league_name + f_league_id + f_home_team_history_league_id + f_away_team_history_league_id
-
-# drop NaN and duplicates
-df = df.dropna()
-df = df.drop_duplicates()
-
-# data processing 
-data_set = df[['id']]
-data_set = data_set.join((df['target']))
-data_set = data_set.join(df[features_boolean].astype(int))
-data_set = data_set.join(df[features_numerical].astype(float))
-
-y = data_set.columns[1]
-x = data_set.columns[2:]
-#target
-Y=data_set[y]
-#train
-X=data_set[x]
 
 #label encoding
 label_encoder = LabelEncoder()
@@ -77,7 +38,7 @@ def train_prediction(config: dict):
     d_train = xgb.DMatrix(X_train, y_train)
     d_test = xgb.DMatrix(X_valid, y_valid)
     results = {}
-    num_round=10
+    num_round=100
     xgb.train(
          config,
          d_train,
@@ -111,8 +72,9 @@ analysis = tune.run(
     mode="min",
     resources_per_trial={"cpu": 1},
     config=config,
-    num_samples=3,
-    verbose=3)
+    num_samples=4,
+    verbose=3,
+    resume=None)
 
 print(f"Best model parameters: {analysis.best_config}")
 print(f" Best achieved loss: {analysis.best_result['mlogloss']}")
